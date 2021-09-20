30c71f7eac395d23c62c4a07ed29c94fbbc5d35f
fedyrahmatullah <48233176+fedyrahmatullah@users.noreply.github.com>
2021-09-20 11:30:26 +0700

    Update ud-fedyjaya.py

--- ud-fedyjaya.py
+++ ud-fedyjaya.py
@@ -20,6 +20,60 @@ def main():
         elif opsiUser == '2':
             lihatBarang()
             break
-            
+    
+def tambahBarang():
+    clear()
+    print("UD.FEDY JAYA")
+    print("Menambahkan Barang")
+    print("------------------")
+    print()
+    print("Apa yang bisa saya bantu?")
+    print()
+    print("1 - Tambahkan lebih dari satu barang")
+    print("2 - Tambahkan satu Barang")
+    print()
+    while True:
+        opsiUser = input("Tentukan Pilihan Anda: ")
+        if opsiUser in ['1', '2']:
+            break
+        if opsiUser == '1':
+            print()
+            while True:
+                noBarang = input("Masukkan jumlah barang yang Ingin ditambahkan: ")
+                if noBarang.isdigit(): #mendeteksi string angka saja
+                    break
+                noBarang = int(noBarang)
+                barangUser = {}
+                for i in range(1, noBarang+1):
+                    while True:
+                        print()
+                        barangToko = input("Nama Barang: ")
+                        if barangToko != '':
+                            break
+                    while True:
+                        jml_Barang = input("Jumlah Barang: ")
+                        if jml_Barang.isdigit():
+                            break
+                    barangUser.update({barangToko: int(jml_Barang)})
+                        
+                tambahBarangKeFile(barangUser, clear=False)
+                keMenu("Barang telah ditambahkan")
+                
+            elif opsiUser == '2':
+                print()
+                while True:
+                    barangToko = input("Nama Barang: ")
+                    if barangToko != '':
+                        break
+                    while True:
+                        jml_Barang = input("Jumlah Barang: ")
+                        if jml_Barang.isdigit():
+                            break
+                    tambahBarangKeFile({barangToko: int(jml_Barang)}, clear=False)
+                    keMenu("Barang telah ditambahkan")
+                    
+                
+                        
+                
         
     
