e5f90523bf2f4d61565705180f010aecb13bfc1f
fedyrahmatullah <48233176+fedyrahmatullah@users.noreply.github.com>
2021-09-20 12:07:14 +0700

    Update ud-fedyjaya.py

--- ud-fedyjaya.py
+++ ud-fedyjaya.py
@@ -36,41 +36,113 @@ def tambahBarang():
         opsiUser = input("Tentukan Pilihan Anda: ")
         if opsiUser in ['1', '2']:
             break
-        if opsiUser == '1':
-            print()
+    if opsiUser == '1':
+        print()
+        while True:
+            noBarang = input("Masukkan jumlah barang yang Ingin ditambahkan: ")
+            if noBarang.isdigit(): #mendeteksi string angka saja
+                break
+        noBarang = int(noBarang)
+        barangUser = {}
+        for i in range(1, noBarang+1):
             while True:
-                noBarang = input("Masukkan jumlah barang yang Ingin ditambahkan: ")
-                if noBarang.isdigit(): #mendeteksi string angka saja
-                    break
-                noBarang = int(noBarang)
-                barangUser = {}
-                for i in range(1, noBarang+1):
-                    while True:
-                        print()
-                        barangToko = input("Nama Barang: ")
-                        if barangToko != '':
-                            break
-                    while True:
-                        jml_Barang = input("Jumlah Barang: ")
-                        if jml_Barang.isdigit():
-                            break
-                    barangUser.update({barangToko: int(jml_Barang)})
-                        
-                tambahBarangKeFile(barangUser, clear=False)
-                keMenu("Barang telah ditambahkan")
-                
-            elif opsiUser == '2':
                 print()
-                while True:
-                    barangToko = input("Nama Barang: ")
-                    if barangToko != '':
-                        break
-                    while True:
-                        jml_Barang = input("Jumlah Barang: ")
-                        if jml_Barang.isdigit():
-                            break
-                    tambahBarangKeFile({barangToko: int(jml_Barang)}, clear=False)
-                    keMenu("Barang telah ditambahkan")
+                barangToko = input("Nama Barang: ")
+                if barangToko != '':
+                    break
+            while True:
+                jml_Barang = input("Jumlah Barang: ")
+                if jml_Barang.isdigit():
+                    break
+            barangUser.update({barangToko: int(jml_Barang)})
+                        
+        tambahBarangKeFile(barangUser, clear=False)
+        keMenu("Barang telah ditambahkan")
+                
+    elif opsiUser == '2':
+        print()
+        while True:
+            barangToko = input("Nama Barang: ")
+            if barangToko != '':
+                break
+        while True:
+            jml_Barang = input("Jumlah Barang: ")
+            if jml_Barang.isdigit():
+                break
+        tambahBarangKeFile({barangToko: int(jml_Barang)}, clear=False)
+        keMenu("Barang telah ditambahkan")
+        
+def lihatBarang():
+    clear()
+    print("UD.FEDY JAYA")
+    print("Lihat Daftar Barang Toko")
+    print("------------------------")
+    print()
+    barangToko = getbarangToko()
+    print("BARANG")
+    print("------")
+    print()
+    for barang in barangToko:
+        print(f"{barang}: {barangToko[barang]}")
+        
+    print()
+    print("UD.FEDY JAYA")
+    print("Opsi yang tersedia: ")
+    print()
+    print("1- Edit Barang")
+    print("2 - Hapus Barang")
+    print()
+    while True:
+        opsiUser = input("Tentukan Pilihan Anda: ")
+        if opsiUser == '1':
+            editBarangToko()
+            break
+        elif opsiUser == '2':
+            hapusBarangToko()
+            break
+
+def editBarangToko():
+    clear()
+    print("UD.FEDY JAYA")
+    print("Edit Barang Toko")
+    print("----------------")
+    print("Tekan (K) untuk Kembali")
+    print()
+    print("Opsi yang tersedia: ")
+    print()
+    print("1 - Edit Nama Barang")
+    print("2 - Edit Jumlah Barang")
+    print()
+    while True:
+        opsiUser = input("Tentukan Pilihan Anda: ").lower()
+        if opsiUser in ['1', '2', 'k']:
+            break
+    if opsiUser == 'k':
+        main()
+        
+    barangToko = getbarangToko()
+    if opsiUser == '1':
+        print()
+        while True:
+            barangDiedit = input("Masukkan Nama Barang yang akan diedit: ")
+            if barangDiedit in barangToko:
+                break
+            else:
+                print("Barang Tidak Ada")
+                print()
+        
+        while True:
+            newNamaBarang = input("Masukkan Nama Baru untuk Barang: ")
+            if newNamaBarang != '':
+                break
+        barangToko.update({newNamaBarang: barangToko[barangDiedit]})
+        del barangToko[barangDiedit]
+        
+        tambahBarangKeFile(barangToko, clear=True)
+        keMenu("Nama barang telah diubah")
+        
+            
+    
                     
                 
                         
